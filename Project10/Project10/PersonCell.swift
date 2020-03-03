73597a7ba656fd3f865a12903af841c796a44fab
Adrian Cordova y Quiroz <a1cordov@ucsd.edu>
2020-03-02 21:53:24 -0700

    Add files via upload
    Project 10: Names to Faces

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by adrian cordova y quiroz on 1/18/20.
+//  Copyright © 2020 adrian cordova y quiroz. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
