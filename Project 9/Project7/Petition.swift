3d93a46485a21caa0d5c44a2b2209db634573bb0
Adrian Cordova y Quiroz <a1cordov@ucsd.edu>
2020-03-02 21:43:33 -0700

    Add files via upload
    Project 9: Grand Central Dispatch

---
+++ Project 9/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by adrian cordova y quiroz on 1/17/20.
+//  Copyright © 2020 adrian cordova y quiroz. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 9/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by adrian cordova y quiroz on 1/17/20.
+//  Copyright © 2020 adrian cordova y quiroz. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
